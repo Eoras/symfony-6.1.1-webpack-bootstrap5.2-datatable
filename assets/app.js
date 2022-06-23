07429244da2bc73d62dad2d433e1e89d38407382
Paul DOS SANTOS <eoras@outlook.fr>
2022-06-23 15:16:10 +0200

    working project

---
+++ assets/app.js
@@ -0,0 +1,60 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+const $ = require('jquery');
+import 'datatables.net-bs5'
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.scss';
+const bootstrap = require('bootstrap');
+
+// start the Stimulus application
+import './bootstrap';
+
+(() => {
+    // BOOTSTRAP JAVASCRIPT POPOVER
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+    const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+
+    // BOOTSTRAP JAVASCRIPT TOOLTIP
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+    // BOOTSTRAP JAVASCRIPT TOASTER
+    const toastTrigger = document.getElementById('liveToastBtn')
+    const toastLiveExample = document.getElementById('liveToast')
+    if (toastTrigger) {
+        toastTrigger.addEventListener('click', () => {
+            const toast = new bootstrap.Toast(toastLiveExample)
+
+            toast.show()
+        })
+    }
+})();
+
+// BOOTSTRAP JAVASCRIPT FORM VALIDATION
+(() => {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+    })
+})();
+
+$(document).ready(function () {
+    $('#example').DataTable();
+});
